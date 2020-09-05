670de9a09d230f660e53f3c0adfe62fb203c64b7
Diego Garcia <diegoalonzo_prod@hotmail.com>
2020-09-05 14:40:31 -0700

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Diego Garcia on 9/5/20.
+//  Copyright © 2020 garciadiegoTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
